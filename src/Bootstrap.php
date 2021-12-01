9279b21a50e6a0cb0ae51429bac78921b8483ca4
Dennison Williams <DennisonWilliams@gmail.com>
2021-11-30 16:41:24 -0800

    Correctly outputs the custom SJI panel on the participant demographics page

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -13,24 +13,29 @@
 
 namespace OpenEMR\Modules\SJIDemographics;
 
+
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
  * Note the below use statements are importing classes from the OpenEMR core codebase
  */
+use OpenEMR\Gacl\Gacl;
 use OpenEMR\Menu\MenuEvent;
 use OpenEMR\Events\RestApiExtend\RestApiCreateEvent;
 use OpenEMR\Events\PatientDemographics\RenderEvent;
+use OpenEMR\Events\PatientFinder\PatientFinderFilterEvent;
+use OpenEMR\Events\AbstractBoundFilter;
+use OpenEMR\Events\BoundFilter;
 
-// TODO: we should bail if the sji_intake forms do not exist
-// idealy we should install those forms with this module
-// we might be able to do this in the code below
-require_once('/interface/forms/sji_intake_core_variables/report.php');
-require_once('/interface/forms/sji_intake/report.php');
-require_once('/interface/forms/sji_stride_intake/report.php');
+include_once($GLOBALS['fileroot'] .'/interface/globals.php');
+include_once($GLOBALS['fileroot'] .'/library/acl.inc');
+include_once($GLOBALS['fileroot'] .'/interface/forms/sji_intake/report.php');
+include_once($GLOBALS['fileroot'] .'/interface/forms/sji_stride_intake/report.php');
+include_once($GLOBALS['fileroot'] .'/interface/forms/sji_intake_core_variables/report.php');
 
 class Bootstrap
 {
+
     const MODULE_INSTALLATION_PATH = "";
     const MODULE_NAME = "sji-demographics";
 	/**
@@ -46,8 +51,61 @@ class Bootstrap
 	public function subscribeToEvents()
 	{
 		$this->eventDispatcher->addListener(
+			PatientFinderFilterEvent::EVENT_HANDLE, 
+			[$this, 'addSJIPatientFinderFilter']);
+
+		$this->eventDispatcher->addListener(
 			RenderEvent::EVENT_SECTION_LIST_RENDER_BEFORE, 
 			[$this, 'addSJIDemographics']);
+	}
+
+	public function addSJIPatientFinderFilter(PatientFinderFilterEvent $event) {
+		// There are 4 different places the search term can come in from 
+		// search_any - from the global patient search filter which does not use 
+		// the dynamic ajax filter.  This is the only one we are going to support
+		$search = $GLOBALS['_REQUEST']['sSearch_0'];
+		if (!strlen($search)) { 
+			$search = $GLOBALS['_REQUEST']['search_any'];
+		}
+
+		if (!strlen($search)) {
+			return;
+		}
+
+		//error_log('search: '. print_r($search, 1));
+		$sql = 'SELECT MAX(id) AS id, pid from form_sji_intake_core_variables '.
+			'where aliases like ? group by pid';
+		//error_log('sql: '. print_r($sql, 1));
+		$res = sqlStatement($sql, array('%'.$search.'%'));
+		$pids = array();
+		while ($row = sqlFetchArray($res)) {
+			//error_log('pid: '. print_r($row['pid'], 1));
+			$pids[] = $row['pid'];
+		}
+
+		// The main search logic checks the begining of the string, we want to match anywhere
+		$sql = 'SELECT pid from patient_data where '.
+			'lname like ? or '.
+			'fname like ? or '.
+			'mname like ? or '.
+			'pid like ? or '.
+			'DOB like ?';
+		$res = sqlStatement($sql, array(
+			'%'.$search.'%',
+			'%'.$search.'%',
+			'%'.$search.'%',
+			'%'.$search.'%',
+			'%'.$search.'%'
+		));
+		while ($row = sqlFetchArray($res)) {
+			//error_log('pid: '. print_r($row['pid'], 1));
+			$pids[] = $row['pid'];
+		}
+
+		if (count($pids)) {
+			$where_clause = 'patient_data.pid in ('. implode(",", $pids) .') or 1 ';
+			$event->getBoundFilter()->setFilterClause($where_clause);
+		}
 	}
 
 	private function getPatientData($pid) {
@@ -73,9 +131,9 @@ class Bootstrap
 
 	// TODO: add js to hide the default demographics "card"
 	public function addSJIDemographics(RenderEvent $event) {
-		$widget = '';
+		$pid = $event->getPid();
 		if (acl_check('patients', 'demo')) { 
-			$widget .= "<tr>\n<td>\n";
+			echo "<section>\n";
 
 			// SJI Demographics expand collapse widget
 			$widgetTitle = xl("SJI Participant");
@@ -99,12 +157,13 @@ class Bootstrap
 			    $fixedWidth
 			);
 
-			$widget .= "<div id=\"SJI\" >\n<ul class=\"tabNav\">\n";
+			echo "<div id=\"SJI\" >\n".
+				"<ul class=\"tabNav\">\n";
 
 			// display tabs for: basic participant info,
 			// and all intakes: main, CV, STRIDE
 
-			$widget .= '<li class="current"> <a href="#" id="header_tab_Who"> Who</a></li>'."\n";
+			echo '<li class="current"> <a href="#" id="header_tab_Who"> Who</a></li>'."\n";
 
 			// TODO: data show when it was last updated?
 
@@ -116,7 +175,7 @@ class Bootstrap
 				$cv_rows['ct'] > 0 && 
 				acl_check('forms', 'intake')
 			) {
-				$widget .= '<li class="">'.
+				echo '<li class="">'.
 					'<a href="#" id="header_tab_CV">Core Variables</a>'.
 					'</li>'."\n";
 			}
@@ -131,7 +190,7 @@ class Bootstrap
 				acl_check('forms', 'intake')
 			) {
 
-				$widget .= '<li class="">'.
+				echo '<li class="">'.
 					'<a href="#" id="header_tab_Intake">'.
 					'Intake</a> </li>'."\n";
 			}
@@ -143,109 +202,104 @@ class Bootstrap
 				isset($stride_rows['ct']) && 
 				$stride_rows['ct'] > 0 && 
 				acl_check('forms', 'intake')) {
-				$widget .= '<li class="">'.
+				echo '<li class="">'.
 					'<a href="#" id="header_tab_Stride">x STRIDE</a>'.
 					'</li>'."\n";
 			}
 
-			$widget .= '</ul>'.
-			  '<div class="tabContainer">'.
-			  '<div class="tab current">'.
-			  '<table border=0 cellpadding=0>'.
-			  '<tbody>';
+			echo '</ul>'. "\n".
+			  '<div class="tabContainer">'. "\n".
+			  '<div class="tab current">'. "\n".
+			  '<table class="table table-borderless">'. "\n".
+			  '<tbody>'. "\n";
 
 			// Get name, gender
-			$patient_data = getPatientData($pid);
-			$gender = getGender($patient_data['sex']);
-			$patient_cv = getCoreVariables($pid);
+			$patient_data = $this->getPatientData($pid);
+			$gender = $this->getGender($patient_data['sex']);
+			$patient_cv = $this->getCoreVariables($pid);
 
-			$widget .= '<tr><td class="label_custom" colspan=1 id="label_title">'.
-				'<span id="label_title">Name:</span></td>'.
+			echo '<tr><td class="label_custom" colspan=1 id="label_title">'.
+				'<span id="label_title" class="bold">Name:</span></td>'.
 				"\n<td class='text data' colspan=1 id=text_title ";
 
 			if (isset($patient_data['title'])) {
-				$widget .= '>'. $patient_data['title'] .' ';
+				echo '>'. $patient_data['title'] .' ';
 
 			}else {
-				$widget .= '>';
+				echo '>';
 			}
 
 			if (isset($patient_data['fname'])) {
-				$widget .= $patient_data['fname'] .' ';
+				echo $patient_data['fname'] .' ';
 			}
 
 			if (isset($patient_data['mname'])) {
-				$widget .= $patient_data['mname'] .' ';
+				echo $patient_data['mname'] .' ';
 			}
 
 			if (isset($patient_data['lname'])) {
-				$widget .= $patient_data['lname'];
+				echo $patient_data['lname'];
 			}
-			$widget .= "</td>\n</tr>\n";
+			echo "</td>\n</tr>\n";
 
 			if (isset($patient_cv['aliases'])) {
-				$widget .= "<tr>\n<td class='label_custom' colspan=1 id='label_aliases'>\n";
-				$widget .= "<span id='label_aliases'>". xl('Aliases') .":</span></td>\n".
+				echo "<tr>\n<td class='label_custom' colspan=1 id='label_aliases'>\n";
+				echo "<span id='label_aliases' class='bold'>". xl('Aliases') .":</span></td>\n".
 				  '<td class="text data" colspan=1 id="text_aliases">';
-				$widget .= $patient_cv['aliases'] ."\n</td>\n</tr>\n";
+				echo $patient_cv['aliases'] ."\n</td>\n</tr>\n";
 			}
 
 			if (isset($patient_cv['pronouns'])) {
-				$widget .= "<tr>\n<td class='label_custom' colspan=1 id='label_pronouns'>\n";
-				$widget .= "<span id='label_pronouns'>". xl('Pronouns') .":</span></td>\n".
+				echo "<tr>\n<td class='label_custom' colspan=1 id='label_pronouns'>\n";
+				echo "<span id='label_pronouns' class='bold'>". xl('Pronouns') .":</span></td>\n".
 				  '<td class="text data" colspan=1 id="text_pronouns">';
-				$widget .= $patient_cv['pronouns'] ."\n</td>\n</tr>\n";
+				echo $patient_cv['pronouns'] ."\n</td>\n</tr>\n";
 			}
 
 			if (isset($gender['title'])) {
-				$widget .= "<tr>\n<td class='label_custom' colspan=1 id='label_sex'>\n";
-				$widget .= "<span id='label_sex'>". xl('Gender') .":</span></td>\n".
+				echo "<tr>\n<td class='label_custom' colspan=1 id='label_sex'>\n";
+				echo "<span id='label_sex' class='bold'>". xl('Gender') .":</span></td>\n".
 				  '<td class="text data" colspan=1 id="text_gender">';
-				$widget .= $gender['title'] ."\n</td>\n</tr>\n";
+				echo $gender['title'] ."\n</td>\n</tr>\n";
 
 			} else if (isset($patient_data['sex'])) {
-				$widget .= "<tr>\n<td class='label_custom' colspan=1 id='label_sex'>\n";
-				$widget .= "<span id='label_sex'>". xl('Gender') .":</span></td>\n".
+				echo "<tr>\n<td class='label_custom' colspan=1 id='label_sex'>\n";
+				echo "<span id='label_sex' class='bold'>". xl('Gender') .":</span></td>\n".
 				  '<td class="text data" colspan=1 id="text_gender">';
-				$widget .= $patient_data['sex'] ."\n</td>\n</tr>\n";
+				echo $patient_data['sex'] ."\n</td>\n</tr>\n";
 			}
 
 			// close of the parent table and div."tab current"
-			$widget .= "</tbody></table></div>\n";
+			echo "</tbody></table></div>\n";
 
 			// create tab for CV
 			if ( isset($cv_rows['ct']) && $cv_rows['ct'] > 0 && acl_check('forms', 'intake')
 			) {
-				$widget .= "<div class='tab'>\n";
-				$widget .= sji_intake_core_variables_reporti_string($pid);
-				$widget .= "</div>\n";
+				echo "<div class='tab'>\n";
+				echo sji_intake_core_variables_report_string($pid);
+				echo "</div>\n";
 			}
 
 			// create tab for Intake
 			if (isset($intake_rows['ct']) && $intake_rows['ct'] > 0 && acl_check('forms', 'intake')
 			) {
-				$widget .= "<div class='tab'>\n";
-				$widget .= sji_intake_report_string($pid);
-				$widget .= "</div>\n";
+				echo "<div class='tab'>\n";
+				echo sji_intake_report_string($pid);
+				echo "</div>\n";
 			}
 
 			// create tab for STRIDE
 			if (isset($stride_rows['ct']) && $stride_rows['ct'] > 0 && acl_check('forms', 'intake')
 			) {
-			   $widget .= "<div class='tab'>\n";
-			   $widget .= sji_stride_intake_report_string($pid);
-			   $widget .= "</div>\n";
+			   echo "<div class='tab'>\n";
+			   echo sji_stride_intake_report_string($pid);
+			   echo "</div>\n";
 			}
 
 			//close off div.tabContainer and div#SJI
-			$widget .= "</div></div>";
+			echo "</div></div>";
 
-			$widget .= '</div>'.
-				'</div>'.
-				'</div>'.
-				'</td>'.
-				'</tr>';
-			print $widget;
+			echo '</section>'."\n";
 		} // if the user has patients demo permission
 	} // function
 
